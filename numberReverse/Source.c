9f716e85f661fce2eb0b1736266b8c673c4e2b7d
Yekta Özdemir <y.ozdemir71@gmail.com>
2017-11-19 23:40:23 +0300

    Updated.

--- numberReverse/Source.c
+++ numberReverse/Source.c
@@ -1,18 +1,38 @@
 #include<stdio.h>
 
+int numberIs(int num);
+int digitIs(int numDigit, int num);
+
 int main()
 {
+	int num = 0, numDigit = 0;
+
+	num = numberIs(num);
+	numDigit = digitIs(numDigit,num);
 
 
-
-
-
-
-
-
-
-
+	
+	printf("num iste%d\n", num);
+	printf("i iste%d\n", numDigit);
 
 	system("PAUSE");
 	return 0;
+}
+
+int numberIs(int num)
+{
+	printf("Enter your number:");
+	scanf_s("%d", &num);
+	
+	return num;
+}
+
+int digitIs(int numDigit, int num)
+{
+	while (num > 0)
+	{
+		num /= 10;
+		numDigit++;
+	}
+	return numDigit;
 }
